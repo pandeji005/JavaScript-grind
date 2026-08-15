7909287b24c36b9198ef6da141a2c9a638136af6
pandeji005 <amoghadeshpande@gmail.com>
2026-08-15 21:59:36 +0530

    datatypes and ecma and mdm docs

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all code as newer version of JavaScript
+
+
+// alert("Welcome to the JavaScript Basics");
+
+/*
+ alert doesnt work in node only works in browser
+*/
+
+console.log(3+3)
+
+console.log("Amogh");
+
+
+let state = null
+
+let age = 18
+/*
+
+primitive data types
+
+ number => 2
+ bigint
+ string => "" ''
+ boolean=> T/F
+ null => standalone value (null can be assigned)
+ undefined => not yet assigned
+ symbol => uniqueness
+*/
+
+console.log(typeof "Amogh")
+console.log(typeof age)
+console.log(typeof null) //object type
+console.log(typeof undefined) //undefined type
+
+
+
