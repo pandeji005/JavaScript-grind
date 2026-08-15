29b3127807920c8c2b0ef02894543d51666ce1f7
pandeji005 <amoghadeshpande@gmail.com>
2026-08-15 22:44:41 +0530

    Operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,21 @@
+let str1 = "Amogh"
+let str2 = " Deshpande"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("2" + 3 + 1) // gives output as 231
+console.log(2 + 3 + "1") // gives output as 51
+
+/*
+ improve code readability
+*/
+
+
+console.log(+true)
+console.log(+"")
+
+let counter = 100
+counter++ //postfix and prefix => n++ , ++n mdn docs
+console.log(counter);
+
